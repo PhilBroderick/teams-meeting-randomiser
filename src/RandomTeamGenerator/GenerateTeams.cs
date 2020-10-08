b0ded64e30295c2e189d403fd65b9a620251b77f
Phil Broderick <phil.broderick@forthcom.co.uk>
2020-10-08 17:22:16 +0100

    Split last two teams so they aren't stacked on one team

--- src/RandomTeamGenerator/GenerateTeams.cs
+++ src/RandomTeamGenerator/GenerateTeams.cs
@@ -17,7 +17,7 @@ namespace RandomTeamGenerator
     public static class GenerateTeams
     {
         [FunctionName("GenerateTeams")]
-        public static async Task<IActionResult> RunAsync(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Admin, "get", Route = null)]
             HttpRequest req, ExecutionContext context, ILogger log)
         {
@@ -29,29 +29,54 @@ namespace RandomTeamGenerator
                 .Build();
 
             var contacts = config["Emails"].Split(',').ToList();
-            var teamCount = int.Parse(config["TeamCount"]);
+
+            if (!int.TryParse(config["TeamCount"], out var teamCount))
+                return new BadRequestObjectResult("No team count defined in configuration.");
             
             var teams = CreateTeams(contacts.OrderBy(s => Guid.NewGuid()), teamCount);
             return teams.Count != 0
                 ? (ActionResult) new OkObjectResult(teams)
-                : new BadRequestObjectResult("No contacts found.");
+                : new BadRequestObjectResult("No contacts found or error creating teams.");
         }
         
         private static List<List<string>> CreateTeams(IEnumerable<string> contacts, int teamCount)
         {
             var output = new List<List<string>>();
             var contactList = contacts.ToList();
-            var teams = Math.Round((decimal) contactList.Count / teamCount);
+            var teams = (decimal) contactList.Count / teamCount;
 
             for (var i = 0; i < teams; i++)
             {
-                var c = contactList.Skip(i * teamCount).Take(teamCount);
-                var team = new List<string>();
-                team.AddRange(c);
-                output.Add(team);
+                var currentTeam = contactList.Skip(i * teamCount).Take(teamCount);
+                output.Add(new List<string>(currentTeam));
             }
+            
+            var splitLastTwoTeamsDueToTeamSize = teams % 1 < 0.5m && teams % 1 != 0;
 
-            return output;
+            return splitLastTwoTeamsDueToTeamSize ? SplitLastTeams(output, teamCount) : output;
+        }
+
+        private static List<List<string>> SplitLastTeams(List<List<string>> teams, int teamCount)
+        {
+            var teamOneToSplit = teams[^2];
+            var teamTwoToSplit = teams[^1];
+                
+            teams.RemoveAt(teams.Count - 2);
+            teams.RemoveAt(teams.Count - 1);
+                
+            var totalCount = teamCount + teamTwoToSplit.Count;
+            var evenSplit = totalCount / 2;
+            var remainder = totalCount % 2;
+            
+            var contactsToMove = teamOneToSplit.Skip(evenSplit + remainder).Take(teamCount - evenSplit).ToList();
+
+            teamTwoToSplit.AddRange(contactsToMove);
+            teamOneToSplit.RemoveRange(evenSplit + remainder, contactsToMove.Count);
+
+            teams.Add(teamOneToSplit);
+            teams.Add(teamTwoToSplit);
+
+            return teams;
         }
     }
 }
